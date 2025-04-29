9991bbd35f0914b031edc3420e7abca3ed970d02
somitmittal <somitmittal@gmail.com>
2025-04-29 15:56:51 +0530

    metrics path fix

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ from app.chat.consumers import ChatConsumer
 from app import views
 
 urlpatterns = [
-    path('/', include('django_prometheus.urls')),
+    path('', include('django_prometheus.urls')),
     path('admin/', admin.site.urls),
     path('health', views.health),
     path('ready', views.ready),
